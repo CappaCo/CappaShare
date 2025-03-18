4c15f82d8985510e739930d66edbe7617c18540f
Casper Howse <22231@northcote.school.nz>
2025-03-18 14:29:37 +1300

    I broke it by changing the style

--- backend/build.ts
+++ backend/build.ts
@@ -2,7 +2,6 @@ console.log("Starting build...");
 
 const sourcePath = "source";
 const buildPath = "build";
-const fileName = "index.html";
 
 const carKeys = {
     "<!-- NAV BAR GOES HERE -->": ["", ""],
@@ -14,11 +13,13 @@ await Deno.remove(buildPath, { recursive: true });
 console.log("Making build directory");
 await Deno.mkdir(buildPath);
 
-const decoder = new TextDecoder("utf-8");
-let content = decoder.decode(await Deno.readFile(`./${sourcePath}/${fileName}`));
+async function buildFile(fileName: String) {
+    const decoder = new TextDecoder("utf-8");
+    let content = decoder.decode(await Deno.readFile(`./${sourcePath}/${fileName}`));
 
-content = content.replace("Lorem", "Yabadabadoo");
+    content = content.replace("Lorem", "Yabadabadoo");
 
-// write new data
-const newdata = new TextEncoder().encode(content);
-await Deno.writeFile(`./${buildPath}/${fileName}`, newdata);
+    // write new data
+    const newdata = new TextEncoder().encode(content);
+    await Deno.writeFile(`./${buildPath}/${fileName}`, newdata);
+}
