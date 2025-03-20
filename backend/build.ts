271e78ced73d5d6cd71e1e4087bbcd22486b773b
Casper Howse <casperhowse@gmail.com>
2025-03-21 10:57:49 +1300

    Make the build system work (not implemented in pages yet)

--- backend/build.ts
+++ backend/build.ts
@@ -2,24 +2,107 @@ console.log("Starting build...");
 
 const sourcePath = "source";
 const buildPath = "build";
+const templatePath = "templates";
 
-const carKeys = {
-    "<!-- NAV BAR GOES HERE -->": ["", ""],
-};
+const decoder = new TextDecoder("utf-8");
+const encoder = new TextEncoder();
 
-console.log("Clearing build directory");
-await Deno.remove(buildPath, { recursive: true });
+try {
+    console.log("Clearing build directory");
+    await Deno.remove(buildPath, { recursive: true });
+} catch (_e) {
+    const e = _e as Error;
+    if (e.name == "NotFound") {
+        console.log("Build directory already clear");
+    }
+    else throw e;
+}
 
 console.log("Making build directory");
 await Deno.mkdir(buildPath);
 
-async function buildFile(fileName: String) {
-    const decoder = new TextDecoder("utf-8");
+function countSpaces(line: string): string {
+    let spaces = "";
+    let index = 0;
+    while (line.charAt(index++) === " ") {
+        spaces += " ";
+    }
+    return spaces;
+}
+
+function addSpaces(lines: string[], spaces: string): string[] {
+    return lines.map((replaceLine) => spaces + replaceLine);
+}
+
+async function replaceInFile(file: string, replace: string): Promise<string> {
+    console.log(`       Replacing ${replace}`);
+    const replaceFile = decoder.decode(await Deno.readFile(`./${templatePath}/${replace}`));
+
+    const lines = file.split("\n");
+    let replaceLines = replaceFile.split("\n");
+
+    const replaceHere = `<!-- ${replace} HERE -->`;
+    const replaceStart = `<!-- ${replace} START -->`;
+    const replaceEnd = `<!-- ${replace} END -->`;
+    const insertHere = `<!-- INSERT HERE -->`;
+
+    let keepReplacing;
+    do {
+        keepReplacing = false;
+
+        // Replace the replaceHere comment
+        const index = lines.findIndex((line) => line.includes(replaceHere));
+        if (index != -1) {
+            keepReplacing = true;
+            console.debug(`           Found ${replaceHere}`);
+            lines.splice(index, 1, ...addSpaces(replaceLines, countSpaces(lines[index])));
+            continue;
+        }
+
+        // Replace the replaceStart and replaceEnd comment
+        const startIndex = lines.findIndex((line) => line.includes(replaceStart));
+        const endIndex = lines.findIndex((line) => line.includes(replaceEnd));
+        if (startIndex != -1 && endIndex != -1) {
+            keepReplacing = true;
+            console.debug(`           Found ${replaceStart} to ${replaceEnd}`);
+            
+            const insides = lines.slice(startIndex + 1, endIndex);
+            const spaces = countSpaces(lines[startIndex]);
+            replaceLines = replaceLines.map((replaceLine) => { if (replaceLine.includes(insertHere)) return insides; else return [replaceLine]; }).flat();
+            
+            lines.splice(startIndex, endIndex - startIndex + 1, ...addSpaces(replaceLines, spaces));
+
+            continue;
+        } else if (!(startIndex != 1 || endIndex != 1)) {
+            console.error(`Start ${startIndex} and end ${endIndex}`);
+            throw new Error("Start without end or end without start");
+        }
+    } while (keepReplacing);
+
+    file = lines.join("\n");
+
+    return file;
+}
+
+async function buildFile(fileName: string) {
+    console.log(`   Building ${fileName}`);
     let content = decoder.decode(await Deno.readFile(`./${sourcePath}/${fileName}`));
 
-    content = content.replace("Lorem", "Yabadabadoo");
+    const fileNames = Deno.readDir(`./${templatePath}`);
+    for await (const file of fileNames) {
+        if (file.isFile) {
+            content = await replaceInFile(content, file.name);
+        }
+    }
 
-    // write new data
-    const newdata = new TextEncoder().encode(content);
+    // Write new data
+    const newdata = encoder.encode(content);
     await Deno.writeFile(`./${buildPath}/${fileName}`, newdata);
+}
+
+for await (const file of Deno.readDir(`./${sourcePath}`)) {
+    if (file.isFile) {
+        // Only build html files
+        if (file.name.endsWith(".html")) await buildFile(file.name);
+    }
 }
