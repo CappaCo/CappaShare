d43f551281a62eb4ec21b55e0e2c0f17a07b98fb
Casper Howse <casperhowse@gmail.com>
2025-03-24 17:34:30 +1300

    Use globalThis

--- source/scripts/main.js
+++ source/scripts/main.js
@@ -1,11 +1,11 @@
-console.log("Main is running");
-// evil sam navbar code
-/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
-let prevScrollpos = window.pageYOffset;
+console.log("Main is running at " + document.URL);
+
+// evil sam navbar code (modified)
+let prevScrollpos = globalThis.pageYOffset;
 const nav = document.getElementsByTagName("nav")[0];
 const hiddenTagName = "hidden";
-window.onscroll = function() {
-    const currentScrollPos = window.pageYOffset;
+globalThis.onscroll = function() {
+    const currentScrollPos = globalThis.pageYOffset;
     if (prevScrollpos > currentScrollPos) {
         nav.classList.remove(hiddenTagName);
     } else {
