f6d3fa4822e938581dda88c052bdc3a7d3935ab1
Casper Howse <casperhowse@gmail.com>
2025-03-24 17:23:58 +1300

    Nav bar hides

--- source/scripts/main.js
+++ source/scripts/main.js
@@ -2,12 +2,14 @@ console.log("Main is running");
 // evil sam navbar code
 /* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
 let prevScrollpos = window.pageYOffset;
+const nav = document.getElementsByTagName("nav")[0];
+const hiddenTagName = "hidden";
 window.onscroll = function() {
-  const currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").style.top = "0";
-  } else {
-    document.getElementById("navbar").style.top = "-50px";
-  }
-  prevScrollpos = currentScrollPos;
+    const currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        nav.classList.remove(hiddenTagName);
+    } else {
+        nav.classList.add(hiddenTagName);
+    }
+    prevScrollpos = currentScrollPos;
 }
