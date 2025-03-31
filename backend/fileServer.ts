eb0378bfb691d796e972a8ab8ea06e5668d078b1
Casper Howse <22231@northcote.school.nz>
2025-04-01 09:58:54 +1300

    Update upload page Create fileServer.ts

---
+++ backend/fileServer.ts
@@ -0,0 +1,30 @@
+console.log("File server running");
+
+// Handle all requests to file server
+async function handler(req: Request) {
+    const reqMethod = req.method;
+    const reqURL = new URL(req.url);
+    const reqPath = reqURL.pathname;
+
+    // If the request method is GET
+    if (reqMethod == "GET") {
+        // Get parts of the website
+        return await getRequest(req);
+    } else if (reqMethod == "POST") {
+        return await postRequest(req);
+    } else {
+        // Otherwise it's a bad request
+        return new Response("Yeah idk", { status: 400 });
+    }
+}
+
+async function getRequest(req: Request) {
+    return new Response("Get request :)");
+}
+
+async function postRequest(req: Request) {
+    return new Response("Post request :)");
+}
+
+// Serve with deno
+Deno.serve(handler);
