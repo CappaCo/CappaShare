7110297be375e03ccf6c1caf40a91ca6c514822e
Casper Howse <22231@northcote.school.nz>
2025-04-03 12:45:11 +1300

    Update upload page

--- backend/fileServer.ts
+++ backend/fileServer.ts
@@ -3,8 +3,8 @@ console.log("File server running");
 // Handle all requests to file server
 async function handler(req: Request) {
     const reqMethod = req.method;
-    const reqURL = new URL(req.url);
-    const reqPath = reqURL.pathname;
+    //const reqURL = new URL(req.url);
+    //const reqPath = reqURL.pathname;
 
     // If the request method is GET
     if (reqMethod == "GET") {
@@ -19,12 +19,14 @@ async function handler(req: Request) {
 }
 
 async function getRequest(req: Request) {
+    console.log("Getting GET request");
     return new Response("Get request :)");
 }
 
 async function postRequest(req: Request) {
+    console.log("Getting POST request");
     return new Response("Post request :)");
 }
 
 // Serve with deno
-Deno.serve(handler);
+Deno.serve({ port: 8001 }, handler);
