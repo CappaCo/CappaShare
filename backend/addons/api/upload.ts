482a09225a32d306485d968b18284a54bcff3aa7
Casper Howse <22231@northcote.school.nz>
2025-05-12 11:00:56 +1200

    Start working on the upload systems

--- backend/addons/api/upload.ts
+++ backend/addons/api/upload.ts
@@ -2,7 +2,8 @@ console.log("upload.ts loaded");
 
 export const path = "/upload";
 
-export function run(_req: Request): Response {
+export function run(req: Request): Response {
+    console.log(req);
     return new Response(
         `hooray!`,
     );
