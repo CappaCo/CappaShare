794a034661660f42d03e93aac550574f516d27ae
Casper Howse <22231@northcote.school.nz>
2025-05-20 11:03:24 +1200

    Update upload api

--- backend/addons/api/upload.ts
+++ backend/addons/api/upload.ts
@@ -2,12 +2,24 @@ console.log("upload.ts loaded");
 
 export const path = "/upload";
 
-export function run(req: Request): Response {
-    const { method, body } = req;
+export async function run(req: Request): Promise<Response> {
+    console.log("file upload incoming");
+    const method = req.method;
     
     if (method != "POST") return new Response("method not allowed", { status: 405 });
 
-    console.log("body: " + body);
+    handleFileUpload(await req.formData());
 
-    return new Response(`hooray!`);
+    return new Response("<a href=\"/upload\">Back to upload</a><br><p>Hooray!</p>", { headers: { "Content-Type": "text/html" }});
+}
+
+function handleFileUpload(data: FormData) {
+    const file = data.get("file");
+
+    console.log("---------------------------------");
+    console.log(data);
+    console.log("title: " + data.get("title"));
+    console.log("description: " + data.get("description"));
+    console.log(file);
+    console.log("---------------------------------");
 }
