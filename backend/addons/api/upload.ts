6596c835e7f25a912164d2e40c06d4978dbf0f96
Casper Howse <22231@northcote.school.nz>
2025-05-21 12:20:34 +1200

    Add upload capabitlities (unsecure)

--- backend/addons/api/upload.ts
+++ backend/addons/api/upload.ts
@@ -8,18 +8,37 @@ export async function run(req: Request): Promise<Response> {
     
     if (method != "POST") return new Response("method not allowed", { status: 405 });
 
+    console.log("---------------------------------");
+    console.group();
     handleFileUpload(await req.formData());
+    console.groupEnd();
+    console.log("---------------------------------");
 
     return new Response("<a href=\"/upload\">Back to upload</a><br><p>Hooray!</p>", { headers: { "Content-Type": "text/html" }});
 }
 
-function handleFileUpload(data: FormData) {
+async function handleFileUpload(data: FormData) {
+    const title = data.get("title");
+    const description = data.get("description");
     const file = data.get("file");
 
-    console.log("---------------------------------");
-    console.log(data);
-    console.log("title: " + data.get("title"));
-    console.log("description: " + data.get("description"));
+    console.log("Handling file upload");
+    console.log("title: " + title);
+    console.log("description: " + description);
+
+    if (file instanceof File) {
+        console.log("Samuel Morresey");
+    } else {
+        console.log("File is not filing rn");
+        return;
+    }
+
+    if (file.size > 100000) {
+        console.log("File too big");
+        return;
+    }
+
     console.log(file);
-    console.log("---------------------------------");
+    const fileFr = new Uint8Array(await file.arrayBuffer());
+    await Deno.writeFile(`./backend/uploads/${file.name}`, fileFr);
 }
